b8a1de06b6a1a103dfbdbbb1f9e51dfa15d5666a
Zabai Armas Herrera <zbrmas@hotmail.com>
2022-11-29 17:45:19 +0000

    feat: add api endpoints

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,6 @@
+// TODO: Change for real endpoint when ready
+export const GET_PODCASTS =
+  "https://run.mocky.io/v3/e7955612-07a5-40a9-8513-14397dd2cb04";
+
+export const GET_PODCAST_DETAILS =
+  "https://run.mocky.io/v3/7643fda7-bfe8-4233-84a5-73b5f8068bc8";
